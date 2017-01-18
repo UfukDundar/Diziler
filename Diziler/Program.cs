9fd329de8d430dde2f0814d432dd7b1e4eb2a01d
ufuk <ufuk-dundar@outlook.com>
2017-01-18 19:40:08 +0300

    Updated.

--- Diziler/Program.cs
+++ Diziler/Program.cs
@@ -55,6 +55,36 @@ namespace Diziler
             //Console.ReadKey();
 
             #endregion
+            #region Dizi4
+
+            //int val = 15;
+            //int[,] dizi = new int[val, val];
+
+            //for (int i = 0; i < val; ++i)
+            //    for(int j=0; j<val; j++)
+            //{
+            //        if(j==i)
+            //        {
+            //            dizi[i, j] = 1;
+            //            dizi[i, val - j - 1] = 1;
+            //        }
+            //        if (i == val / 2)
+            //            dizi[i, j] = 1;
+            //        if (j == val / 2)
+            //            dizi[i, j] = 1;
+            //}
+            //for(int i =0; i<val;++i)
+            //{
+            //    for (int j = 0; j < val; j++)
+            //    {
+            //        if (dizi[i, j] == 1)
+            //            Console.Write("*");
+            //        else
+            //            Console.Write(" ");
+            //    }
+            //}
+            Console.ReadKey();
+            #endregion
 
         }
     }
