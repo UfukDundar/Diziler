9e3a7e31bf79034fdbfb8f5f59a91157ea5d88f7
ufuk <ufuk-dundar@outlook.com>
2017-01-18 14:54:58 +0300

    Updated.

--- Diziler/Program.cs
+++ Diziler/Program.cs
@@ -10,6 +10,52 @@ namespace Diziler
     {
         static void Main(string[] args)
         {
+            #region Dizi1
+
+            //int[] dizi1 = new int[10];
+            //bool[] dizi2 = new bool[10];
+            //string[] dizi3 = new string[10];
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write("{0,5}", dizi1[i]);
+            //    Console.Write("{0,6}", dizi2[i]);
+            //    Console.WriteLine("{0,5}", dizi3[i]);
+            //}
+            //Console.ReadKey();
+            #endregion
+            #region Dizi2
+            //Console.Write("Dinamik dizinin boyutu: ");
+            //int val = Convert.ToInt32(Console.ReadLine());
+            //int[] dizi = new int[val];
+
+            //for (int i = 0; i < val; i++)
+            //{
+            //    Console.WriteLine(dizi[i]);
+            //}
+            //Console.ReadKey();
+            #endregion
+            #region Dizi3
+            //Random rnd = new Random();
+            //int[] RasgeleDizi = new int[100];
+
+            //for (int i = 0; i < 100; ++i)
+
+            //    RasgeleDizi[i] = rnd.Next(1, 11);
+            //int[] adet=new int[10];
+
+            //for (int i = 0; i < 100; ++i)
+            //    adet[RasgeleDizi[i] - 1]++;
+            //for(int i=0; i<10; ++i)
+            //{
+            //    Console.Write("{0,2} Adet {1,2}-->",adet[i], i + 1);
+            //    for (int j = 0; j < adet[i]; ++j)
+            //        Console.Write("*");
+            //    Console.WriteLine();
+            //}
+            //Console.ReadKey();
+
+            #endregion
+
         }
     }
 }
